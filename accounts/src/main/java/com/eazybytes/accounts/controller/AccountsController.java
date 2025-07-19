c636242c6df6fbb493e5014fd0f9cca86c0eb84d
u97 <atul.bajare@3ds.com>
2025-07-19 23:06:34 +0530

    @Environment => reading system properties

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -15,6 +15,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +34,9 @@ public class AccountsController {
 
     @Autowired
     private IAccountsService iAccountsService;
+
+    @Autowired
+    private Environment environment;
 
     @GetMapping(value = "/hello")
     public String sayHello(){
@@ -187,5 +191,29 @@ public class AccountsController {
     @GetMapping("/build-info")
     public String getBuildInfo(){
         return buildVersion;
+    }
+
+
+    @Operation(
+            summary = "Get java info",
+            description = "Get java info"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/java-version")
+    public String getJavaVersion(){
+        return environment.getProperty("JAVA_HOME");
     }
 }
